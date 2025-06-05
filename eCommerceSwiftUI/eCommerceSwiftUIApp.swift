4e5db7e993aa2b22ed69059520b842aec614e1de
Bonmyeong Koo - vn56eqd <Bonmyeong.Koo@walmart.com>
2025-06-05 18:21:45 -0400

    Initial Commit

---
+++ eCommerceSwiftUI/eCommerceSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  eCommerceSwiftUIApp.swift
+//  eCommerceSwiftUI
+//
+//  Created by Bonmyeong Koo - Vendor on 6/5/25.
+//
+
+import SwiftUI
+
+@main
+struct eCommerceSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
